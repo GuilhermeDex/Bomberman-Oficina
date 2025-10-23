e2fbb62b672eabca82e4881553e85895e4d9137a
Guilherme Francis <130943190+GuilhermeDex@users.noreply.github.com>
2025-10-23 16:15:25 -0300

    Delete teste/Scripts directory

--- teste/Scripts/power_up_res.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-class_name PowerUpRes
-
-@export var type: Utils.PowerUpType
-@export var texture: Texture
--- teste/Scripts/utils.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-enum PowerUpType {
-	BOMB_UP,
-	FIRE_UP,
-	SPEED_UP,
-}
